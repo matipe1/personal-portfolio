b679c0ed908004c68a6641a0dc473712604bfa5b
Diego Petitto <dmpetitto2004@gmail.com>
2025-12-17 23:38:45 -0300

    Initialize personal portfolio project with React, Vite, and TypeScript

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import styles from './Home.module.css';
+
+export const Home = () => {
+  return (
+    <div className={styles.container}>
+        {/* Theme Toggle */}
+
+        {/* Background Effects */}
+
+        {/* Navbar */}
+
+        {/* Main Content */}
+
+        {/* Footer */}
+        Home Component
+    </div>
+  );
+};
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+import styles from './NotFound.module.css';
+
+export const NotFound = () => {
+    return (
+        <div className={styles.container}>
+            NotFound Component
+        </div>
+    )
+};
