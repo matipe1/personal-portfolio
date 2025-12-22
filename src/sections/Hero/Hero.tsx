de6ea8b0433df2f5970de213c386f6d02190d88b
Diego Petitto <dmpetitto2004@gmail.com>
2025-12-22 13:28:01 -0300

    Add Hero section with styles and content

---
+++ src/sections/Hero/Hero.tsx
@@ -0,0 +1,20 @@
+import { SentenceRotator } from '../../components/SentenceRotator/SentenceRotator';
+import styles from './Hero.module.css';
+
+export const Hero = () => {
+
+    return(
+        <section className={styles.heroContainer}>
+            <div>
+                <h1 className={styles.title}>Hi, I'm Diego</h1>
+                <h2 className={styles.subtitle}>
+                    <SentenceRotator />
+                </h2>
+                <p className={styles.description}>Advanced Engineering Student and eager to get a work on the field</p>
+            </div>
+            <div>
+                🟦 (logo)
+            </div>
+        </section>
+    );
+}
